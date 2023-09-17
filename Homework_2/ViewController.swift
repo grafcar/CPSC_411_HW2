00c5cf232ceaed1e1c520aca2168d22d1981a455
Bryan De Los Santos <bdelossantos1@csu.fullerton.edu>
2023-09-17 16:41:19 -0700

    Initial commit

---
+++ Homework_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2
+//
+//  Created by Bryan De Los Santos on 9/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
