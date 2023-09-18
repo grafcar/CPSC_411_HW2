0850e35f90dd866e78b4ca9bfc91acc50e4fb45b
Bryan De Los Santos <bdelossantos1@csu.fullerton.edu>
2023-09-17 18:16:54 -0700

    Made the connections to all buttons and labels

--- Homework_2/ViewController.swift
+++ Homework_2/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showNextQuestion(_ sender: UIButton){
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton){
+        
+    }
 
 }
 
